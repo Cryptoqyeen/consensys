4600afa99740a67d8b5703edc02a785a3db13759
Michgolden Ukeje <mich.u@bettercast.io>
2023-06-17 15:01:31 +0100

    Update img size and handle responsiveness

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ import "./home.css";
 export default function Home() {
   return (
     <section>
-      <main className="flex min-h-screen flex-col items-center justify-between p-10 main-wrap">
+      <main className="flex min-h-screen flex-col items-center justify-between md:p-10 p-3 main-wrap">
         <Navbar />
         <MainIntro />
         <About />
